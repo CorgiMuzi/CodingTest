6869c3ac0518a6881af7640c0094d82b345131da
CorgiMuzi <kjhcorgi99@gmail.com>
2021-11-03 18:24:49 +0900

    소수 만들기 문제 시작

--- CodingTest/MakingPrimeNumber/MakingPrimeNumber.cpp
+++ CodingTest/MakingPrimeNumber/MakingPrimeNumber.cpp
@@ -1,3 +1,22 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
+int solution(vector<int> nums)
+{
+	int answer = -1;
+
+
+
+	return answer;
+}
+
+int main(void)
+{
+	vector<int> nums = { 1,2,3,4 };
+
+	solution(nums);
+
+	return 0;
+}
